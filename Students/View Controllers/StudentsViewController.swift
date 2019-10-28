55a9d2be4dfb517ca4890fdf2ffe9d2cd75301ba
Nar Kumar <njn.kumar@gmail.com>
2019-10-28 12:19:07 -0700

    Sorting and Filter almost done

--- Students/View Controllers/StudentsViewController.swift
+++ Students/View Controllers/StudentsViewController.swift
@@ -17,8 +17,31 @@ class StudentsViewController: UIViewController {
     
     // MARK: - Properties
 
+    private let studentController = StudentController()
+    
+    private var filterAndSortedStudents: [Student] = [] {
+        didSet {
+            tableView.reloadData()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
+        
+        studentController.loadFromPersistentStore(completetion: {students, error in
+            if let error = error {
+                print("Error loading students: \(error)")
+            }
+            
+            // go over 75min mark to 90 min mark to review this concept again
+            DispatchQueue.main.sync {
+                if let students = students {
+                    self.filterAndSortedStudents = students
+                }
+            }
+            
+        })
 
     }
     
@@ -35,13 +58,16 @@ class StudentsViewController: UIViewController {
 
 extension StudentsViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return filterAndSortedStudents.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "StudentCell", for: indexPath)
         
-        // Configure cell
+
+        let aStudent = filterAndSortedStudents[indexPath.row]
+        cell.textLabel?.text = aStudent.name
+        cell.detailTextLabel?.text = aStudent.course
         
         return cell
     }
